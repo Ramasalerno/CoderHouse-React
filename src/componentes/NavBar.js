3477e966c3821de62a72dbae8848ab98bad2ba98
Ramiro Salerno <salernoramiro94@gmail.com>
2021-11-07 22:35:36 -0300

    Desafio n3 navbar

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,23 @@
+
+
+const NavBar = () => {
+
+    return ( 
+
+            <div>
+                <header>
+                         <nav className="menu">
+                             <ul className="menu_items">
+                                <li>Inicio</li>
+                                <li>Galeria</li>
+                                <li>¿Quienes somos?</li>
+                                <li>Contacto</li>
+                             </ul>
+                    </nav>
+                </header>
+
+            </div>
+    )
+}
+
+export default NavBar
