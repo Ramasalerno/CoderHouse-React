123a73694ac8ae387cc43c071a609d13cfb6383f
Ramiro Salerno <salernoramiro94@gmail.com>
2021-11-15 22:53:18 -0300

    Desafio n5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-/* import { ItemCount } from './componentes/ItemCount/ItemCount'; */
 import { ItemListContainer } from './componentes/ItemListContainer/ItemListContainer';
 import { NavBar } from './componentes/NavBar/NavBar';
-/* import { TarjetaProducto } from './componentes/TarjetaProduct/TarjetaProducto'; */
 
 
 function App() {
